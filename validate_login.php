0d43a054ac7a8b5c83f5dd300239fefed7cd4d83
James Saballegue <jp.saballegue@gmail.com>
2020-02-12 20:11:33 +0800

    a lil polish

--- validate_login.php
+++ validate_login.php
@@ -2,15 +2,13 @@
     session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $bool = true;
 
-    $mysqli_link = mysqli_connect("localhost", "phpmyadmin", "root", "mydb") or die (mysqli_error()); //Connect to server
-    // mysqli_select_db("mydb") or die ("Cannot connect to database");          //Connect to database
+    $mysqliLink = mysqli_connect("localhost", "phpmyadmin", "root", "mydb") or die (mysqli_error()); //Connect to server
 
-    $result = mysqli_query(mysqli_link, "SELECT * FROM users WHERE username='$username' AND password=MD5('$password')"); // Query the users table
+    $result = mysqli_query($mysqliLink, "SELECT * FROM users WHERE username='$username' AND password=MD5('$password')", MYSQLI_NUM); // Query the users table
+
     $isExists = $result === TRUE;
-    $row = mysqli_fetch_array($result, MYSQLI_NUM);
-
+    
     if($isExists) //IF there are no returning rows or no existing username
     {
         Print '<script>alert("Success!");</script>'; // Prompts the user
